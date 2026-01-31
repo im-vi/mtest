e615281f7c7bb626cb8af0c9b55dc1ef00b81687
vi <79205177+im-vi@users.noreply.github.com>
2026-01-31 23:13:08 +0100

    sa

---
+++ content.js
@@ -0,0 +1,93 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const site = urlParams.get('site');
+const dir = urlParams.get('dir');
+
+async function loadHTMLPage(page) {
+    fetch(`sites/${page}.html`)
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById("content").innerHTML = html;
+    })
+    .catch(err => console.error("Nie można załadować strony HTML:", err));
+}
+
+async function getFolder(owner, repo, path = "") {
+    const url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+  
+    const response = await fetch(url);
+    if (!response.ok) {
+        document.getElementById("content").innerHTML = `<div class="container"><h1>Błąd ${response.status}</h1></div>`;
+        throw new Error(`Błąd: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data.map(item => ({
+        name: item.name,
+        type: item.type,
+    }));
+}
+
+async function loadDirectoryPage(directory) {
+    let result = await getFolder("im-vi", "SejmTracker", directory);
+    console.log(result);
+    let files = [];
+    let directories = [];
+    result.forEach(element => {
+        switch(element.type) {
+            case "file": files.push(element.name); break;
+            case "dir": directories.push(element.name); break;
+        }
+    });
+
+    console.log(files);
+    console.log(directories);
+
+    let container = document.createElement("div");
+    container.className = "container";
+    let list = document.createElement("ul");
+    list.className = "list-group list-group-flush";
+    files.forEach(item => {
+        let listItem = document.createElement("li");
+        listItem.className = "list-group-item";
+        listItem.innerHTML = `<a href="files/${directory}/${item}">${item}</a>`
+        list.appendChild(listItem);
+    })
+    container.appendChild(list);
+    if(directories.length != 0) {
+        directories.forEach(item => {
+            let newDirHeading = document.createElement("h2");
+            newDirHeading.className = "mt-3";
+            newDirHeading.textContent = item;
+            let nList = document.createElement("ul");
+            nList.className = "list-group list-group-flush";
+            getFolder("im-vi", "SejmTracker", `${directory}/${item}`)
+            .then(json => {
+                json.forEach(item2 => {
+                    let listItem = document.createElement("li");
+                    listItem.className = "list-group-item";
+                    listItem.innerHTML = `<a href="files/${directory}/${item}/${item2.name}">${item2.name}</a>`
+                    nList.appendChild(listItem);
+                })
+            })
+            container.appendChild(newDirHeading);
+            container.appendChild(nList);
+        })
+    }
+        
+    document.getElementById("content").appendChild(container);
+}
+
+async function showContent() {
+    if(!site && !dir) {
+        loadHTMLPage("home");
+    } else if(site && !dir) {
+        loadHTMLPage(site);
+    } else if(!site && dir) {
+        loadDirectoryPage(dir);
+    } else {
+        document.getElementById("content").innerHTML = `<div class="container"><h1>Za dużo argumentów.</h1><br><a class="btn btn-primary" href="/" role="button"><i class="bi bi-house-door"></i> Wróć do strony głównej</a></div>`;
+    }
+}
---
+++ navbar.js
@@ -0,0 +1,85 @@
+let nav = document.createElement("nav");
+let content = `<div class="container-fluid">
+            <a class="navbar-brand" href="/"><img src="zst.png" height="50px" alt="ZST Puławy"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Pracownie Techniczne
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="?dir=PracownieTechniczne/regulamin-pracowni-technicznej"><i class="bi bi-arrow-return-right"></i> Regulamin pracowni technicznej</a></li>
+                            <li><a class="dropdown-item" href="?dir=PracownieTechniczne/dokumenty-do-druku"><i class="bi bi-arrow-return-right"></i> Dokumenty do druku</a></li>
+                            <li><a class="dropdown-item" href="?dir=PracownieTechniczne/literatura"><i class="bi bi-arrow-return-right"></i> Literatura</a></li>
+                            <li><a class="dropdown-item" href="?dir=PracownieTechniczne/pracownia-rysunku-technicznego"><i class="bi bi-arrow-return-right"></i> Pracownia rysunku technicznego</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Klasa czwarta
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="?dir=KlasaCzwarta/01-badania"><i class="bi bi-arrow-return-right"></i> Badania właściwości metali i stopów</a></li>
+                            <li><a class="dropdown-item" href="?dir=KlasaCzwarta/02-pomiary"><i class="bi bi-arrow-return-right"></i> Pomiary warsztatowe</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Klasa piąta
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="?dir=KlasaPiata/01-opracowanie-konstrukcyjne"><i class="bi bi-arrow-return-right"></i> Opracowanie konstrukcyjne zespołu<br>(specyfikacja, materiały oraz rysunki<br>wykonawcze)</a></li>
+                            <li><a class="dropdown-item" href="?dir=KlasaPiata/02-opracowanie-technologiczne"><i class="bi bi-arrow-return-right"></i> Opracowanie technologiczne<br>(karta technologiczna, instrukcji operacji)</a></li>
+                            <li><a class="dropdown-item" href="?dir=KlasaPiata/03-opracowanie-technologii-montazu"><i class="bi bi-arrow-return-right"></i> Opracowanie technologii montażu i<br>demontażu zespołu (schemat poglądowy<br>i blokowy)</a></li>
+                            <li><a class="dropdown-item" href="?dir=KlasaPiata/04-opracowanie-technologiczne-naprawy"><i class="bi bi-arrow-return-right"></i> Opracowanie technologiczne naprawy<br>(remontu) zespołu</a></li>
+                            <li><a class="dropdown-item" href="?dir=KlasaPiata/05-zakres-prac-remontow"><i class="bi bi-arrow-return-right"></i> Zakres prac remontów: bieżącego,<br>średniego i kapitalnego</a></li>
+                            <li><a class="dropdown-item" href="?dir=KlasaPiata/06-koszt-remontow"><i class="bi bi-arrow-return-right"></i> Koszt remontu bieżącego i kapitalnego</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Zajęcia Teoretyczne
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="?dir=ZajeciaTeoretyczne/01-podstawy-konstrukcji-maszyn"><i class="bi bi-arrow-return-right"></i> Podstawy konstrukcji maszyn</a></li>
+                            <li><a class="dropdown-item" href="?dir=ZajeciaTeoretyczne/02-podstawy-technik-wytwarzania"><i class="bi bi-arrow-return-right"></i> Podstawy technik wytwarzania</a></li>
+                            <li><a class="dropdown-item" href="?dir=ZajeciaTeoretyczne/03-technologia-montazu-maszyn"><i class="bi bi-arrow-return-right"></i> Technologia montażu maszyn i urządzeń</a></li>
+                            <li><a class="dropdown-item" href="?dir=ZajeciaTeoretyczne/04-maszynoznawstwo"><i class="bi bi-arrow-return-right"></i> Maszynoznawstwo</a></li>
+                            <li><a class="dropdown-item" href="?dir=ZajeciaTeoretyczne/05-organizacja-procesow-produkcji"><i class="bi bi-arrow-return-right"></i> Organizacja procesów produkcji</a></li>
+                            <li><a class="dropdown-item" href="?dir=ZajeciaTeoretyczne/06-podstawy-nadzorowania-pracy-maszyn"><i class="bi bi-arrow-return-right"></i> Podstawy nadzorowania pracy maszyn<br>i urządzeń stosowanych w przemyśle<br>chemicznym</a></li>
+                            <li><a class="dropdown-item" href="?dir=ZajeciaTeoretyczne/07-konstrukcje-maszyn"><i class="bi bi-arrow-return-right"></i> Konstrukcje maszyn</a></li>
+                            <li><a class="dropdown-item" href="?dir=ZajeciaTeoretyczne/08-eksploatacja-maszyn"><i class="bi bi-arrow-return-right"></i> Eksploatacja maszyn i urządzeń przemysłu<br>technicznego</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Egzaminy zawodowe
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="?dir=EgzaminyZawodowe/01-mec03"><i class="bi bi-arrow-return-right"></i> Kwalifikacja MEC.03</a></li>
+                            <li><a class="dropdown-item" href="?dir=EgzaminyZawodowe/02-mec09"><i class="bi bi-arrow-return-right"></i> Kwalifikacja MEC.09</a></li>
+                            <li><a class="dropdown-item" href="?dir=EgzaminyZawodowe/03-informator"><i class="bi bi-arrow-return-right"></i> Informator o egzaminie</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="?site=OWT">OWT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="?site=SzkolneKoloTechniki">Szkolne koło techniki</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="?site=OWOW">OWOW</a>
+                    </li>
+                </ul>
+            </div>
+        </div>`;
+
+nav.innerHTML = content;
+nav.className = "navbar navbar-expand-xl bg-body-tertiary";
+document.body.appendChild(nav)
